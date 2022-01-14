b0c3754902ac597846ad6193a485333c936a669d
tian2009min@163.com <tian2009min@163.com>
2022-01-14 14:20:21 +0800

    Initial commit

---
+++ Solution.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+/**
+ * @author shkstart
+ * @date 2022/1/14 - 10:10
+ */
+public class Solution {
+    /**
+     * 示例
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
+}
